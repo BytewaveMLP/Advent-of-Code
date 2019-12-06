3886b0890314ab778b828c20ed123ace02f77ba3
Eliot Partridge <shortcircuit369@gmail.com>
2019-12-05 23:28:54 -0600

    Remove unnecessary intcode import

--- Day 6/part2.py
+++ Day 6/part2.py
@@ -1,7 +1,5 @@
 import sys
 import os
-sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from intcode import Intcode
 
 class OrbitingBody():
 	def __init__(self, name):
