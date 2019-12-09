2f443951041d08f6f2f81a5fa91bc89e04d6913d
Eliot Partridge <shortcircuit369@gmail.com>
2019-12-09 00:23:39 -0600

    Day 9, improved Intcode interpreter

---
+++ Day 9/part2.py
@@ -0,0 +1,16 @@
+import sys
+import os
+sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+from intcode import Intcode
+
+if len(sys.argv) < 2:
+	print(f'usage: {os.path.basename(sys.argv[1])} <input.txt>')
+	exit(1)
+
+with open(sys.argv[1], 'r') as f:
+	data = f.read().split(',')
+
+ic = Intcode([int(n) for n in data])
+ic.input.append(2)
+ic.run_until_complete()
+print(ic.output)
--- intcode/intcode.py
+++ intcode/intcode.py
@@ -1,23 +1,29 @@
+from collections import defaultdict
+
 class Intcode():
 	def __init__(self, tape):
-		self.tape = tape
+		self.tape = defaultdict(int, enumerate(tape))
 		self.input = []
 		self.output = []
 		self.addr_modes = []
 		self.pc = 0
+		self.rel_base = 0
+		self.running = True
 
-	@property
-	def running(self):
-		return self.pc < len(self.tape)
-	
-	def get_arg(self, n):
+	def get_arg_addr(self, n):
 		addr = self.tape[self.pc + n]
 		mode = self.addr_modes[n - 1]
 
-		if mode == 1: return addr
-		elif mode == 0: return self.tape[addr]
+		if   mode == 2: return self.rel_base + addr
+		elif mode == 1: return addr
+		elif mode == 0: return addr
 		else:
 			raise ValueError(f'Argument mode {mode} not implemented')
+	
+	def get_arg(self, n):
+		addr = self.get_arg_addr(n)
+		mode = self.addr_modes[n - 1]
+		return addr if mode == 1 else self.tape[addr]
 	
 	def step(self):
 		if not self.running: return False
@@ -39,7 +45,7 @@ class Intcode():
 		add a, b, dest
 		"""
 
-		self.tape[self.tape[self.pc+3]] = self.get_arg(1) + self.get_arg(2)
+		self.tape[self.get_arg_addr(3)] = self.get_arg(1) + self.get_arg(2)
 		self.pc += 4
 
 	def intcode_inst_2(self):
@@ -47,7 +53,7 @@ class Intcode():
 		mul a, b, dest
 		"""
 
-		self.tape[self.tape[self.pc+3]] = self.get_arg(1) * self.get_arg(2)
+		self.tape[self.get_arg_addr(3)] = self.get_arg(1) * self.get_arg(2)
 		self.pc += 4
 
 	def intcode_inst_3(self):
@@ -56,7 +62,7 @@ class Intcode():
 		"""
 
 		if len(self.input) == 0: return
-		self.tape[self.tape[self.pc+1]] = self.input.pop(0)
+		self.tape[self.get_arg_addr(1)] = self.input.pop(0)
 		self.pc += 2
 
 	def intcode_inst_4(self):
@@ -64,7 +70,7 @@ class Intcode():
 		out a
 		"""
 
-		self.output.append(self.tape[self.tape[self.pc+1]])
+		self.output.append(self.get_arg(1))
 		self.pc += 2
 
 	def intcode_inst_5(self):
@@ -92,9 +98,10 @@ class Intcode():
 		lt  a, b, dest
 		"""
 
-		self.tape[self.tape[self.pc+3]] = 0
+		out_val = 0
 		if self.get_arg(1) < self.get_arg(2):
-			self.tape[self.tape[self.pc+3]] = 1
+			out_val = 1
+		self.tape[self.get_arg_addr(3)] = out_val
 		self.pc += 4
 
 	def intcode_inst_8(self):
@@ -102,17 +109,26 @@ class Intcode():
 		eq  a, b, dest
 		"""
 
-		self.tape[self.tape[self.pc+3]] = 0
+		out_val = 0
 		if self.get_arg(1) == self.get_arg(2):
-			self.tape[self.tape[self.pc+3]] = 1
+			out_val = 1
+		self.tape[self.get_arg_addr(3)] = out_val
 		self.pc += 4
+
+	def intcode_inst_9(self):
+		"""
+		arb off
+		"""
+
+		self.rel_base += self.get_arg(1)
+		self.pc += 2
 	
 	def intcode_inst_99(self):
 		"""
 		hlt
 		"""
 
-		self.pc = len(self.tape)
+		self.running = False
 
 	def intcode_inst_not_found(self):
 		raise ValueError(f'Instruction {self.tape[self.pc]} not found at pc={self.pc}')
